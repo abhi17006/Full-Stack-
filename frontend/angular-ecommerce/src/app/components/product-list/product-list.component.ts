94ae90256d8c7514252966f1a4f88b39750e054f
Abhi <abhi17patel@gmail.com>
2024-02-21 13:09:27 -0500

    add HTML contents

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
 
+  products: Product[] = [];
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.listProducts();  
+  }
+
+  listProducts() {
+    //fetch data from Service Methods API
+    this.productService.getProductList().subscribe(
+      data =>{
+        this.products = data;
+    });
+  }
 }
--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = "http://localhost:8080/api/products";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList() : Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+}
+interface GetResponse{
+  // unwraps the JSON from Spring Data REST _embedded entry
+  _embedded:{
+    products: Product[];
+  }
 }
