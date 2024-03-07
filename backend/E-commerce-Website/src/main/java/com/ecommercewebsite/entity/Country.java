f9b6d145c5d6bd30ff955995915eaa77cc1630b5
Abhi <abhi17patel@gmail.com>
2024-03-07 14:43:45 -0500

    add Etities country and state with OneToMany and ManyToOne Mappings

---
+++ backend/E-commerce-Website/src/main/java/com/ecommercewebsite/entity/Country.java
@@ -0,0 +1,29 @@
+package com.ecommercewebsite.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    //one-to-many with States
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+}
