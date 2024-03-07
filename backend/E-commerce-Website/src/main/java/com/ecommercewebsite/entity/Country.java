deaf490bb13d8c66f41c2d2d45d63f7db405dd9d
Abhi <abhi17patel@gmail.com>
2024-03-07 15:05:37 -0500

    add Repositories for country and state with endPoints

--- backend/E-commerce-Website/src/main/java/com/ecommercewebsite/entity/Country.java
+++ backend/E-commerce-Website/src/main/java/com/ecommercewebsite/entity/Country.java
@@ -1,5 +1,6 @@
 package com.ecommercewebsite.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,5 +26,6 @@ public class Country {
 
     //one-to-many with States
     @OneToMany(mappedBy = "country")
+    @JsonIgnore //ignore the state column in the reslutset
     private List<State> states;
 }
