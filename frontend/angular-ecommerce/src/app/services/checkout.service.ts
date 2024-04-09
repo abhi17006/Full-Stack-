fee54d13922bb4165c1ced550b796e7430036662
Abhi <abhi17patel@gmail.com>
2024-04-09 15:46:22 -0400

    add stripe payment url, and backend post method for paymentIntent

--- frontend/angular-ecommerce/src/app/services/checkout.service.ts
+++ frontend/angular-ecommerce/src/app/services/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +15,18 @@ export class CheckoutService {
 
   // private purchaseUrl = 'http://localhost:8080/api/checkout/purchase';
 
+  //stripe payment url
+  private paymentIntentUrl = environment['luv2shopApiUrl'] + '/checkout/payment-intent'; 
+
   constructor(private httpClient: HttpClient) { }
 
   placeOrder(purchase: Purchase): Observable<any>{
 
     return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+//method for stripe payment Intent
+  createPaymentIntent(paymentInfo: PaymentInfo): Observable<any> {
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl,paymentInfo);
   }
 }
