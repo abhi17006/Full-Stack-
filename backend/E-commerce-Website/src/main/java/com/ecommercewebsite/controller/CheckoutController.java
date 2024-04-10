64feda12a74fd5d0e6c69c0bbd44139131fd85a6
Abhi <abhi17patel@gmail.com>
2024-04-09 21:53:54 -0400

    add stripe secretKey, add logger for debug purpose

--- backend/E-commerce-Website/src/main/java/com/ecommercewebsite/controller/CheckoutController.java
+++ backend/E-commerce-Website/src/main/java/com/ecommercewebsite/controller/CheckoutController.java
@@ -5,15 +5,20 @@ import com.ecommercewebsite.dto.Purchase;
 import com.ecommercewebsite.dto.PurchaseResponse;
 import com.ecommercewebsite.service.CheckoutService;
 import com.stripe.model.PaymentIntent;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
 
 
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
+    //setup logger
+    private Logger logger = Logger.getLogger(getClass().getName());
     private CheckoutService checkoutService;
 
     public CheckoutController(CheckoutService checkoutService) {
@@ -28,7 +33,7 @@ public class CheckoutController {
 
     @PostMapping("/payment-intent")
     public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo){
-
+        //logger.info("paymentInfo.amount : "+paymentInfo.getAmount());
         PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
 
         String paymentstr = paymentIntent.toJson();
